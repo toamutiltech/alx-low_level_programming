8ff1ca19495d2cb6978c2a6f38f2664e59a387d0
Oluwaseun Adeolu Okunola <oluwaseunokunola@gmail.com>
2022-09-08 16:41:16 -0700

    Hello, puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,8 +1,8 @@
 #include <stdio.h>
-
+/* Making use of puts function */
 int main(void)
 {
-	puts("Programming is like building a multilingual puzzle");
+	puts("\"Programming is like building a multilingual puzzle");
 	return (0);
 
 }
