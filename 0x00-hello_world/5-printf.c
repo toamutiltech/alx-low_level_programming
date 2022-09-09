bd7da9abbc8f7326861222bce086a5aff09e5896
Oluwaseun Adeolu Okunola <oluwaseunokunola@gmail.com>
2022-09-08 17:41:31 -0700

    Hello, printf

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+/**
+ * main - Introduction to Printf Function
+ * printf - print statment
+ * Return: 0 parameters
+ *
+ */
+int main(void)
+{
+	prinf("with proper grammar, but the outcome is a piece of art,\n");
+	return (0);
+}
