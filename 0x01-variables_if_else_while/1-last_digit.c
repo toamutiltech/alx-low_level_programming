8a6c06650e31363fa03388afe009192dffd92982
Oluwaseun Adeolu Okunola <oluwaseunokunola@gmail.com>
2022-09-12 19:21:43 -0700

    The last digit

---
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -0,0 +1,39 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+/* more headers goes there */
+
+/**
+ *  * main - if else statement to test for negativity or positivity
+ *  * srand - pick random numbers
+ *  * if else: to test for positivity or negativity
+ *  * printf - print statment
+ *  * Return: 0 parameters
+ */
+int main(void)
+{
+	/*Declearing variable n */
+	int n = 0;
+
+	srand(time(0));
+
+	n = rand() - RAND_MAX / 2;
+
+	if (n > 5)
+	{
+		printf("%d and is greater than 5\n", n);
+	}
+	else if (n == 0)
+	{
+		printf("%d and is 0\n", n);
+	}
+	else if (n > 0 < 6)
+	{
+		printf("%d and is les than 6 and not 0\n", n);
+	}
+	else
+	{
+		printf("%d is less than 0\n", n);
+	}
+	return (0);
+}
