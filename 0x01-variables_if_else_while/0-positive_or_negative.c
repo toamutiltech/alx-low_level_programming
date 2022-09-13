ff25eb1b56b0151b0d1656d68012a93b48d2b916
Oluwaseun Adeolu Okunola <oluwaseunokunola@gmail.com>
2022-09-12 18:31:51 -0700

    Positive anything is better than negative nothing

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -14,22 +14,21 @@
 int main(void)
 {
 	int n;
-
 	srand(time(0));
 
 	n = rand() - RAND_MAX / 2;
 
 	if (n > 0)
 	{
-		printf("is positive,\n");
+		printf("%d, is positive,\n", n);
 	}
 	else if (n == 0)
 	{
-		printf("is zero,\n");
+		printf("%d, is zero,\n", n);
 	}
 	else
 	{
-		printf("is nagative,\n");
+		printf("%d, is nagative,\n", n);
 	}
 	return (0);
 }
