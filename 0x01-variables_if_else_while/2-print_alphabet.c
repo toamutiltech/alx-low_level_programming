c240ffc896751b2f3a4fe2872840f457f50704e3
Oluwaseun Adeolu Okunola <oluwaseunokunola@gmail.com>
2022-09-12 21:09:08 -0700

    I sometimes suffer from insomnia. And when I can't fall asleep, I play what I call the alphabet game

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+/* more headers goes there */
+
+/**
+ * main - if else statement to test for negativity or positivity
+ * putchar - print statment
+ * Return: 0 parameters
+ */
+int main(void)
+{
+	for (int c = 97; c <= 122; ++c)
+		putchar("%c ", c);
+	putchar("\n");
+	return (0);
+}
