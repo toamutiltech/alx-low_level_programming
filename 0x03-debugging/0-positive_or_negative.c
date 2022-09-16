f5ccf24bdb0858fc7edeaebabeec52e5f35f88cd
Oluwaseun Adeolu Okunola <oluwaseunokunola@gmail.com>
2022-09-16 08:08:00 -0700

    Multiple mains

---
+++ 0x03-debugging/0-positive_or_negative.c
@@ -0,0 +1,35 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+/* more headers goes there */
+
+/**
+ *  * main - if else statement to test for negativity or positivity
+ *  * srand - pick random numbers
+ *  * if else: to test for positivity or negativity
+ *  * printf - print statment
+ *  * Return: 0 parameters
+ */
+int main(void)
+{
+	/*Declearing variable n */
+	int n = 0;
+
+	srand(time(0));
+
+	n = rand() - RAND_MAX / 2;
+
+	if (n > 0)
+	{
+		printf("%d is positive\n", n);
+	}
+	else if (n == 0)
+	{
+		printf("%d is zero\n", n);
+	}
+	else
+	{
+		printf("%d is negative\n", n);
+	}
+	return (0);
+}
