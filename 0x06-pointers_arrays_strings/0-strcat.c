906afa930d104c10295873815cbe61bab2ad8f3c
Oluwaseun Adeolu Okunola <oluwaseunokunola@gmail.com>
2022-09-22 02:52:55 -0700

    strcat

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -1,12 +1,10 @@
 #include "main.h"
-
 /**
  * _strcat - function commute srtings
  * @dest: param pointer to a char
  * @src: param pointer to a char
  * Return: return value of dest
  */
-
 char *_strcat(char *dest, char *src)
 {
 	int i;
