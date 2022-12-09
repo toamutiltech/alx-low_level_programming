92a446e5666ecbb489d5897d6683b7fa674bbf1f
Oluwaseun Adeolu Okunola <oluwaseunokunola@gmail.com>
2022-12-09 09:17:28 -0800

    last digit

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -26,13 +26,13 @@ int main(void)
 	{
 		printf("Last digit of %d is %d and is greater than 5\n", n, l);
 	}
-	else if (l == 0)
-	{
-		printf("Last digit of %d is %d and is 0\n", n, l);
-	}
 	else if (l < 6)
 	{
 		printf("Last digit of %d is %d and is less than 6 and not 0\n", n, l);
+	}
+	else if (l == 0)
+	{
+		printf("Last digit of %d is %d and is 0\n", n, l);
 	}
 	else
 	{
