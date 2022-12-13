9d3a7da2a45a71f5a07e28b5e8990e7c633e265f
Oluwaseun Adeolu Okunola <oluwaseunokunola@gmail.com>
2022-12-13 08:29:26 -0800

    print_alphabet_x10

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -1,20 +1,24 @@
-#include "main.c"
+#include "main.h"
+
 /**
-* print_alphabet_x10 - func
-*Return - 0
+* print_alphabet_x10 - function that prints 10 times the alphabet.
+* Return: Always return 0
 */
- void print_alphabet_x10(void)
+
+void print_alphabet_x10(void)
 {
 	int b = 0;
+
 	while (b < 10)
 	{
-	       char a = 'a';
-	       while (a <= 'z')
-	       {
-		       _putchar(a);
-		       a++;
-	       }
-	       _putchar('\n');
-	       b++;
+		char a = 'a';
+
+		while (a <= 'z')
+		{
+			_putchar(a);
+			a++;
+		}
+		_putchar('\n');
+		b++;
 	}
 }
