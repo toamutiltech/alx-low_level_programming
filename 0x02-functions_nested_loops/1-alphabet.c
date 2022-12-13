467737269abb729bf5e4c203249253a72703a869
Oluwaseun Adeolu Okunola <oluwaseunokunola@gmail.com>
2022-12-13 08:18:03 -0800

    alphet

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -1,18 +1,16 @@
 #include <stdio.h>
 #include "main.h"
-/***
+/**
  * print_alphabet - prints the alphabet, in lowercase, followed by a new line.
- *
  */
 void print_alphabet(void)
 {
 	char alp = 'a';
-	
+
 	while (alp <= 'z')
 	{
 		_putchar (alp);
 		alp++;
-	
 	}
 	_putchar ('\n');
 }
