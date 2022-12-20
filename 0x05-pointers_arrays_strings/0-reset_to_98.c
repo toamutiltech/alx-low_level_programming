4a2752c25ce88ccd6e494e83508527ad1c423675
Oluwaseun Adeolu Okunola <oluwaseunokunola@gmail.com>
2022-12-20 06:02:36 -0800

    reset to 98

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,11 @@
+#include "main.h"
+/**
+ * reset_to_98 - change the value of n to 98
+ * @n: the number to change
+ */
+
+void reset_to_98(int *n)
+{
+	n = &n;
+	*n = 98;
+}
---
+++ 0x05-pointers_arrays_strings/main.h
@@ -0,0 +1,4 @@
+#include "_putchar.c"
+
+void reset_to_98(int *n);
+int _putchar(char c)
