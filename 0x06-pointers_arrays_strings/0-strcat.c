7ad9c9409378156d9c45941fbd30b088c182da15
Oluwaseun Adeolu Okunola <oluwaseunokunola@gmail.com>
2022-12-23 05:46:48 -0800

    first one

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -5,23 +5,15 @@
  * @src: param pointer to a char
  * Return: return value of dest
  */
-char *_strcat(char *dest, char *src)
+char *strcat(char *dest, const char *src)
 {
-	int i;
-	int j;
-	
-	i = 0;
-	j = 0;
-	while (dest[i] != '\0')
-	{
-		i++;
-	}
-	
-	while (src[j] != '\0')
-	{
-		dest[i] = src[j];
-		i++;
-	}
-	dest[i] = '\0';
+	int index = 0, dest_len = 0;
+
+	while (dest[index++])
+		dest_len++;
+
+	for (index = 0; src[index]; index++)
+		dest[dest_len++] = src[index];
+
 	return (dest);
 }
