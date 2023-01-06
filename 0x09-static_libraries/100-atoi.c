a96a4fefa76ac86873d03074f5c4018930a66046
Oluwaseun Adeolu Okunola <oluwaseunokunola@gmail.com>
2023-01-06 14:45:49 -0800

    A library is not a luxury but one of the necessities of life

---
+++ 0x09-static_libraries/100-atoi.c
@@ -0,0 +1,10 @@
+#include "main.h"
+/**
+ * _atoi - function commute srtings
+ * @*s: param pointer to a char
+ * Return: return value of dest
+ */
+int _atoi(char *s)
+{
+
+}
---
+++ 0x09-static_libraries/2-strlen.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * _strlen - Returns the length of a string.
+ * @s: The string to get the length of.
+ *
+ * Return: The length of @str.
+ */
+int _strlen(char *s)
+{
+	size_t length = 0;
+
+	while (*s++)
+		length++;
+
+	return (length);
+}
