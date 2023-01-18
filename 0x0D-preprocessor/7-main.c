05d291f23f2460374d60b9cca335c2fc99ade0cd
Oluwaseun Adeolu Okunola <oluwaseunokunola@gmail.com>
2023-01-17 20:47:43 -0800

    preproccesor

---
+++ 0x0D-preprocessor/7-main.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+/**
+ * main - check the code
+ *
+ * Return: Always 0.
+ */
+int main(void)
+{
+    int i;
+    int j;
+
+    i = -10;
+    j = -10;
+    printf("%d\n", i * j);
+    return (0);
+}
