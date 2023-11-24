7be2e556917a1d09d22eee431ceeb579d88c2438
Oluwaseun Adeolu Okunola <oluwaseunokunola@gmail.com>
2023-11-24 13:57:45 -0800

    1-print_binary.c correction

--- 0x14-bit_manipulation/2-get_bit.c
+++ 0x14-bit_manipulation/2-get_bit.c
@@ -8,10 +8,10 @@
  */
 int get_bit(unsigned long int n, unsigned int index)
 {
-        int bit;
+	int bit;
 
-        bit = (n >> index);
-        if (index > 32)
-        return (-1);
-        return (bit & 1);
+	bit = (n >> index);
+	if (index > 32)
+		return (-1);
+	return (bit & 1);
 }
